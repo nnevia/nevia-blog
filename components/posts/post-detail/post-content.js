c3dca1fca704ed57488695513391fea893813179
nevia <dbs00024@gmail.com>
2024-11-19 23:03:26 +0900

    포스트 추가, 파이썬 하이라이터 추가

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -6,9 +6,11 @@ import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
 import atomDark from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
 import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
 import css from "react-syntax-highlighter/dist/cjs/languages/prism/css";
+import python from "react-syntax-highlighter/dist/cjs/languages/prism/python";
 
 SyntaxHighlighter.registerLanguage("js", js);
 SyntaxHighlighter.registerLanguage("css", css);
+SyntaxHighlighter.registerLanguage("python", python);
 
 export default function PostContent(props) {
   const { post } = props;
