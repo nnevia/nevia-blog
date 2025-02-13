b5d8f84f28fa9d0ed02f0f6a693f4a736b9990f1
nevia <dbs00024@gmail.com>
2025-02-13 20:15:51 +0900

    fix/redirect

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import FeaturedPosts from "../components/home-page/featured-posts";
 import Hero from "../components/home-page/hero";
 import { getFeaturedPosts } from "../lib/post-util";
-
+import { redirect } from "next";
 export default function HomePage(props) {
   // return (
   //   <>
